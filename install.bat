323adb08eae9003e1899ff349597c479b2005a6c
jackjack <root@waka.site40.net>
2011-08-23 21:32:40 +0200

    missing word

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@ if %PROCESSOR_ARCHITECTURE%==AMD64 set arch= (x86)
 if %PROCESSOR_ARCHITECTURE%==IA64 set arch= (x86)
 
 if not exist "C:\Program Files%arch%\GnuWin32\bin\wget.exe" wget-1.11.4-1-setup.exe
-if exist "C:\Program Files%arch%\GnuWin32\bin\wget.exe" wget-1.11.4-1-setup.exe
+if exist "C:\Program Files%arch%\GnuWin32\bin\wget.exe" del wget-1.11.4-1-setup.exe
 
 if not exist "C:\Python27\python.exe" "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://www.python.org/ftp/python/2.7.2/python-2.7.2.msi
 if not exist "C:\Python27\Scripts\easy_install.exe" "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11.win32-py2.7.exe
