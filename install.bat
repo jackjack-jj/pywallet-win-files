5c15556de3d850edd148bbcd063393f0d080f608
jackjack <root@waka.site40.net>
2011-08-24 16:29:47 +0200

    no check certificate for zope.interface@github

--- install.bat
+++ install.bat
@@ -8,7 +8,7 @@ if exist "C:\Program Files%arch%\GnuWin32\bin\wget.exe" del wget-1.11.4-1-setup.
 if not exist "C:\Python27\python.exe" "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://www.python.org/ftp/python/2.7.2/python-2.7.2.msi
 if not exist "C:\Python27\Scripts\easy_install.exe" "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11.win32-py2.7.exe
 if not exist "C:\Python27\Scripts\twistd.py" "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://buildbot.twistedmatrix.com/builds/twisted-packages/Twisted-11.0.0.winxp32-py2.7.exe
-if not exist "C:\Python27\Lib\site-packages\zope.interface-3.6.4-py2.7-win32.egg\zope\__init__.py" "C:\Program Files%arch%\GnuWin32\bin\wget.exe" https://github.com/downloads/jackjack-jj/pywallet-win-files/zope.interface-3.7.0.win32-py2.7.exe
+if not exist "C:\Python27\Lib\site-packages\zope.interface-3.6.4-py2.7-win32.egg\zope\__init__.py" "C:\Program Files%arch%\GnuWin32\bin\wget.exe" https://github.com/downloads/jackjack-jj/pywallet-win-files/zope.interface-3.7.0.win32-py2.7.exe --no-check-certificate
 
 
 if not exist "C:\Python27\python.exe" msiexec /i python-2.7.2.msi
