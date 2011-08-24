bae5afd8e786ce62ce99a500d76f0c53b9a23348
jackjack-jj <root@waka.site40.net>
2011-08-24 09:14:16 +0300

    Edited update.bat via GitHub

--- update.bat
+++ update.bat
@@ -5,12 +5,10 @@ if %PROCESSOR_ARCHITECTURE%==IA64 set arch= (x86)
 del pywallet.py 
 "C:\Program Files%arch%\GnuWin32\bin\wget.exe" https://raw.github.com/jackjack-jj/pywallet/master/pywallet.py --no-check-certificate 
 
-del update.bat
-"C:\Program Files%arch%\GnuWin32\bin\wget.exe" https://raw.github.com/jackjack-jj/pywallet-win-files/master/update.bat --no-check-certificate 
-
 del pywallet.bat
 "C:\Program Files%arch%\GnuWin32\bin\wget.exe" https://raw.github.com/jackjack-jj/pywallet-win-files/master/pywallet.bat --no-check-certificate 
 
 del install.bat
 "C:\Program Files%arch%\GnuWin32\bin\wget.exe" https://raw.github.com/jackjack-jj/pywallet-win-files/master/install.bat --no-check-certificate 
 
+del update.bat & "C:\Program Files%arch%\GnuWin32\bin\wget.exe" https://raw.github.com/jackjack-jj/pywallet-win-files/master/update.bat --no-check-certificate 
