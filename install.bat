fd280bd5129b544f22ff3acdde524760083efad0
jackjack-jj <pentak@live.fr>
2013-04-09 20:57:55 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ if exist "C:\Program Files%arch%\GnuWin32\bin\wget.exe" del wget-1.11.4-1-setup.
 
 if not exist "C:\Python27\python.exe" del python-2.7.2.msi & "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://www.python.org/ftp/python/2.7.2/python-2.7.2.msi
 if not exist "C:\Python27\Scripts\easy_install.exe" del setuptools-0.6c11.win32-py2.7.exe & "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11.win32-py2.7.exe
-if not exist "C:\Python27\Scripts\twistd.py" del Twisted-11.0.0.winxp32-py2.7.exe & "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://buildbot.twistedmatrix.com/builds/twisted-packages/Twisted-11.0.0.winxp32-py2.7.exe
+if not exist "C:\Python27\Scripts\twistd.py" del Twisted-13.0.0.win32-py2.7.exe & "C:\Program Files%arch%\GnuWin32\bin\wget.exe" http://twistedmatrix.com/Releases/Twisted/13.0/Twisted-13.0.0.win32-py2.7.exe
 if not exist "C:\Python27\Lib\site-packages\zope.interface-3.7.0-py2.7.egg-info\requires.txt" del zope.interface-3.7.0.win32-py2.7.exe & "C:\Program Files%arch%\GnuWin32\bin\wget.exe" https://github.com/downloads/jackjack-jj/pywallet-win-files/zope.interface-3.7.0.win32-py2.7.exe --no-check-certificate
 
 
@@ -15,8 +15,8 @@ if not exist "C:\Python27\python.exe" msiexec /i python-2.7.2.msi
 if exist "C:\Python27\python.exe" del python-2.7.2.msi
 setuptools-0.6c11.win32-py2.7.exe
 if exist "C:\Python27\Scripts\easy_install.exe" del setuptools-0.6c11.win32-py2.7.exe
-Twisted-11.0.0.winxp32-py2.7.exe
-if exist "C:\Python27\Scripts\twistd.py" del Twisted-11.0.0.winxp32-py2.7.exe
+Twisted-13.0.0.win32-py2.7.exe
+if exist "C:\Python27\Scripts\twistd.py" del Twisted-13.0.0.win32-py2.7.exe
 zope.interface-3.7.0.win32-py2.7.exe
 if exist "C:\Python27\Lib\site-packages\zope.interface-3.7.0-py2.7.egg-info\requires.txt" del zope.interface-3.7.0.win32-py2.7.exe
 
